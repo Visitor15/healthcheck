6a9816210ebe40cc2e852270f47465ebf3fad0f1
Nick C <visitor15@gmail.com>
2014-11-09 09:41:21 -0500

    Added a test location listener

--- android/Health/wear/src/main/java/mobile/forged/com/health/MainActivity.java
+++ android/Health/wear/src/main/java/mobile/forged/com/health/MainActivity.java
@@ -1,11 +1,27 @@
 package mobile.forged.com.health;
 
 import android.app.Activity;
+import android.location.Location;
 import android.os.Bundle;
 import android.support.wearable.view.WatchViewStub;
+import android.util.Log;
 import android.widget.TextView;
 
-public class MainActivity extends Activity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.Result;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.wearable.Wearable;
+
+public class MainActivity extends Activity implements
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        LocationListener {
+
+    private GoogleApiClient mGoogleApiClient;
 
     private TextView mTextView;
 
@@ -20,5 +36,72 @@ public class MainActivity extends Activity {
                 mTextView = (TextView) stub.findViewById(R.id.text);
             }
         });
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(LocationServices.API)
+                .addApi(Wearable.API)  // used for data layer API
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+    }
+
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mGoogleApiClient.disconnect();
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        LocationRequest locationRequest = LocationRequest.create()
+                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+                .setInterval(250)
+                .setFastestInterval(50);
+
+        LocationServices.FusedLocationApi
+                .requestLocationUpdates(mGoogleApiClient, locationRequest, MainActivity.this)
+                .setResultCallback(new ResultCallback() {
+
+                    @Override
+                    public void onResult(Result result) {
+                        if (result.getStatus().isSuccess()) {
+//                            mTextView.setText(result.getStatus().toString());
+//                            if (Log.isLoggable("MainActivity", Log.DEBUG)) {
+//                                Log.d("MainActivity", "Successfully requested location updates");
+//                                mTextView.setText(result.getStatus().toString());
+//                            }
+                        } else {
+                            Log.e("MainActivity",
+                                    "Failed in requesting location updates, "
+                                            + "status code: "
+                                            + result.getStatus().getStatusCode()
+                                            + ", message: "
+                                            + result.getStatus().getStatusMessage());
+                        }
+                    }
+                });
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        mTextView.setText(location.toString());
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
     }
 }
